249ed78e11c41c02b1c66dc189405154db6ede3f
Elmer Gustavo Pú <elmergustavo79@gmail.com>
2021-04-04 18:59:29 -0600

    Modificado body

--- sistema/index.php
+++ sistema/index.php
@@ -3,7 +3,7 @@
 <div class="container-fluid">
 	<!-- Page Heading -->
 	<div class="d-sm-flex align-items-center justify-content-between mb-4">
-		<h1 class="h3 mb-0 text-gray-800">Restaurante Alex y Alejandra</h1>
+		<h1 class="h3 mb-0 text-gray-800">Panel de Administración</h1>
 	</div>
 
 	<!-- Content Row -->
